35542413030dfd8bf5a59852de445b363328cc25
syaafiudinm <altafpasallo12@gmail.com>
2024-11-28 23:55:55 +0800

    listing and paginate data

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,46 @@
-export default function Home({ name }) {
+import { Link } from "@inertiajs/react";
+
+export default function Home({ posts }) {
     return (
         <>
-            <h1 className="title">Hello {name}</h1>
+            <h1 className="title">Hello</h1>
+            <div>
+                {posts.data.map((post) => (
+                    <div
+                        key={post.id}
+                        className="p-5 border-b border-slate-300"
+                    >
+                        <div className="text-sm text-slate-600">
+                            <span>Posted on : </span>
+                            <span>
+                                {new Date(post.created_at).toLocaleTimeString()}
+                            </span>
+                        </div>
+                        <p className="font-medium">{post.body}</p>
+                    </div>
+                ))}
+            </div>
+
+            <div className="py-12 px-4">
+                {posts.links.map((link) =>
+                    link.url ? (
+                        <Link
+                            key={link.label}
+                            href={link.url}
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                            className={`p-1 mx-1 ${
+                                link.active ? "text-blue-500 font-bold" : ""
+                            }`}
+                        />
+                    ) : (
+                        <span
+                            key={link.label}
+                            dangerouslySetInnerHTML={{ __html: link.label }}
+                            className="p-1 mx-1 text-slate-300"
+                        ></span>
+                    )
+                )}
+            </div>
         </>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return Inertia::render('Home', ['name' => 'Adhim']);
-});
+Route::get('/',[PostController::class,'index']);
+
+Route::resource('posts', PostController::class)->except('index');
